2581ee85ebfe62e9e260a19464f4734efc41d9ba
Pierre <58070376+PierreADev@users.noreply.github.com>
2022-05-09 14:31:35 +0200

    fix: port for heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 5000;
 
 const router = require('./app/router');
 
